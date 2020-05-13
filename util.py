8802ee464198293b6c41025c5624f0b4ab3306d4
alexanalyticsander <58505988+alexanalyticsander@users.noreply.github.com>
2020-05-13 00:38:36 -0700

    Update util.py

--- util.py
+++ util.py
@@ -6,7 +6,7 @@ def entropy(class_y):
     # Input:            
     #   class_y         : list of class labels (0's and 1's)
     
-    # TODO: Compute the entropy for a list of classes
+    # Compute the entropy for a list of classes
     #
     # Example:
     #    entropy([0,0,0,1,1,1,1,1,1]) = 0.92
@@ -37,14 +37,8 @@ def partition_classes(X, y, split_attribute, split_val):
     #   split_attribute : column index of the attribute to split on
     #   split_val       : either a numerical or categorical value to divide the split_attribute
     
-    # TODO: Partition the data(X) and labels(y) based on the split value - BINARY SPLIT.
-    # 
-    # You will have to first check if the split attribute is numerical or categorical    
-    # If the split attribute is numeric, split_val should be a numerical value
-    # For example, your split_val could be the mean of the values of split_attribute
-    # If the split attribute is categorical, split_val should be one of the categories.   
-    #
-    # You can perform the partition in the following way
+    # Partition the data(X) and labels(y) based on the split value - BINARY SPLIT.
+
     # Numeric Split Attribute:
     #   Split the data X into two lists(X_left and X_right) where the first list has all
     #   the rows where the split attribute is less than or equal to the split value, and the 
@@ -95,8 +89,7 @@ def partition_classes(X, y, split_attribute, split_val):
     
     y_left = []
     y_right = []
-    ### Implement your code here
-    #############################################
+
     if isinstance(X[0][split_attribute], str):
         for i in range(len(X)):
             if X[i][split_attribute] == split_val:
@@ -113,7 +106,7 @@ def partition_classes(X, y, split_attribute, split_val):
             else:
                 X_right.append(X[i])
                 y_right.append(y[i])
-    #############################################
+
     return (X_left, X_right, y_left, y_right)
 
     
@@ -123,9 +116,9 @@ def information_gain(previous_y, current_y):
     #   current_y:  the distribution of labels after splitting based on a particular
     #               split attribute and split value
     
-    # TODO: Compute and return the information gain from partitioning the previous_y labels
+    # Compute and return the information gain from partitioning the previous_y labels
     # into the current_y labels.
-    # You will need to use the entropy function above to compute information gain
+
     # Reference: http://www.cs.cmu.edu/afs/cs.cmu.edu/academic/class/15381-s06/www/DTs.pdf
     
     """
@@ -151,12 +144,11 @@ def best_split(X, y):
     # Inputs:
     #   X       : Data containing all attributes
     #   y       : labels
-    # TODO: For each node find the best split criteria and return the 
+    # For each node find the best split criteria and return the 
     # split attribute, spliting value along with 
     # X_left, X_right, y_left, y_right (using partition_classes)
     '''
-    
-    NOTE: Just like taught in class, don't use all the features for a node.
+
     Repeat the steps:
 
     1. Select m attributes out of d available attributes
@@ -167,8 +159,7 @@ def best_split(X, y):
     split_attribute = 0
     split_value = 0
     X_left, X_right, y_left, y_right = [], [], [], []
-    ### Implement your code here
-    #############################################
+
     split_point = 0
     info_gain = 0
     attributes = np.random.randint(0,len(X[0]),4)
@@ -185,4 +176,3 @@ def best_split(X, y):
     X_left, X_right, y_left, y_right = partition_classes(X, y, split_attribute, split_point)
     
     return (split_attribute, split_point, X_left, X_right, y_left, y_right)
-    #############################################
